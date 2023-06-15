1562c001fbd6b6d81dcd08a7d979bfe6ef7a44db
unknown <VLandhaeusser@Veronika.qui.zz>
2023-06-15 10:52:41 +0200

    Level 3 - Bar Setup

---
+++ Assets/Brick Project Studio/Bar Pub Tavern Kit/Common/Scripts and Animations/FlipOpenClose.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SojaExiles
+{
+
+	public class FlipOpenClose : MonoBehaviour
+	{
+
+		public Animator Flip;
+		public bool open;
+		public Transform Player;
+
+		void Start()
+		{
+			open = false;
+		}
+
+		void OnMouseOver()
+		{
+			{
+				if (Player)
+				{
+					float dist = Vector3.Distance(Player.position, transform.position);
+					if (dist < 15)
+					{
+						if (open == false)
+						{
+							if (Input.GetMouseButtonDown(0))
+							{
+								StartCoroutine(opening());
+							}
+						}
+						else
+						{
+							if (open == true)
+							{
+								if (Input.GetMouseButtonDown(0))
+								{
+									StartCoroutine(closing());
+								}
+							}
+
+						}
+
+					}
+				}
+
+			}
+
+		}
+
+		IEnumerator opening()
+		{
+			print("you are opening the door");
+			Flip.Play("FlipOpen");
+			open = true;
+			yield return new WaitForSeconds(.5f);
+		}
+
+		IEnumerator closing()
+		{
+			print("you are closing the door");
+			Flip.Play("FlipClose");
+			open = false;
+			yield return new WaitForSeconds(.5f);
+		}
+
+
+	}
+
+}
---
+++ Assets/Brick Project Studio/Bar Pub Tavern Kit/Common/Scripts and Animations/tapS.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SojaExiles
+{
+
+    public class tapS : MonoBehaviour
+    {
+
+        public Animator TapPour;
+        public Transform Player;
+        public bool pouring;
+
+        void Start()
+        {
+            pouring = false;
+        }
+
+        void OnMouseOver()
+        {
+            {
+                if (Player)
+                {
+                    float dist = Vector3.Distance(Player.position, transform.position);
+                    if (dist < 15)
+                    {
+                        if (pouring == false)
+                        {
+                            if (Input.GetMouseButtonDown(0))
+                            {
+                                TapPour.Play("Pour");
+                                pouring = true;
+                                print("you are pouring");
+                            }
+
+                        }
+                    }
+                }
+
+            }
+
+        }
+
+        void OnMouseExit()
+        {
+            {
+                if (pouring == true)
+                {
+                    TapPour.Play("Pour1");
+                    pouring = false;
+                }
+            }
+        }
+
+    }
+}
